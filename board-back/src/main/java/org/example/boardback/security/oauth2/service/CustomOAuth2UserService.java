5e33baf7a0284283f57ca4c2aa5e646660996e76
cgung7 <cgung7@naver.com>
2025-11-24 16:14:26 +0900

    remove secret properties

--- board-back/src/main/java/org/example/boardback/security/oauth2/service/CustomOAuth2UserService.java
+++ board-back/src/main/java/org/example/boardback/security/oauth2/service/CustomOAuth2UserService.java
@@ -24,12 +24,32 @@ import java.util.Map;
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
+    // User Entity를 Spring Security에서 사용하는 UserPrincipal로 변환하는 매퍼
     private final UserPrincipalMapper userPrincipalMapper;
 
+    /**
+     * OAuth2 로그인 시 provider(google, kakao, naver)로부터 사용자 정보를 가져와
+     *  , 프로젝트 서비스의 User 엔티티로 저장/업데이트 한 뒤
+     *  >> 최종적으로, Spring Security가 이해할 수 있는 OAuth2User(UserPrincipal) 행태로 리턴
+     * */
     @Override
     @Transactional
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        // 1. provider로 부터 사용자 정보 가져오기
+        OAuth2User oAuth2User = super.loadUser(userRequest);
 
+        // 2. registrationId로 provider 구분
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();
+        AuthProvider provider = mapProvider(registrationId);
+
+        // 3. provider 별로 attributes를 공통 포맷으로 변환
+        OAuth2UserInfo userInfo = convertToUserInfo(provider, oAuth2User.getAttributes());
+
+        // 4. DB에 사용자 생성/수정
+        User user = upsertUser(provider, userInfo);
+
+        // 5. 프로젝트 서비스에서 사용하는 UserPrincipal로 래핑
+        return userPrincipalMapper.toPrincipal(user.getUsername());
     }
 
     private AuthProvider mapProvider(String registrationId) {
