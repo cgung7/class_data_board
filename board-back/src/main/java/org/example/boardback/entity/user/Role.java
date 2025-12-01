a45f36ce9a23c39357f47cb53b734de621b3ad23
cgung7 <cgung7@naver.com>
2025-12-01 14:17:05 +0900

    1201 payment

--- board-back/src/main/java/org/example/boardback/entity/user/Role.java
+++ board-back/src/main/java/org/example/boardback/entity/user/Role.java
@@ -4,7 +4,7 @@ import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.example.boardback.common.enums.user.RoleType;
+import org.example.boardback.common.enums.RoleType;
 
 @Entity
 @Table(name = "roles")
